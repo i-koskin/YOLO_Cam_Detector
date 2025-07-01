94f45314f6b631edd105a595d4023998f0d44feb
Igor Koskin <ikoskin1@gmail.com>
2025-07-01 08:42:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
-    logging.info("🛑 Захват остановлен. Окна закрыты")
+    logging.info("🛑 Захват остановлен")
 
 
 if __name__ == "__main__":
